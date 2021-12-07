e29ac260e3e423bd85e4f01e9518da2f29c9597a
oshirojk88 <jcksoshiro@gmail.com>
2021-12-06 22:07:06 -1000

    add tests

--- .gitignore/startFlaskr.ps1
+++ .gitignore/startFlaskr.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-venv\scripts\activate
+.gitignore\venv\scripts\activate
 $env:FLASK_APP = "flaskr"
 $env:FLASK_ENV = "development"
 flask run
